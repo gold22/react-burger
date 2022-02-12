2e71b197e440eb09ea4b992304e9538a60c58c13
Denis Goleshchikhin <dgoleshchikhin@yandex.ru>
2022-02-13 00:06:50 +0700

    Add unit-tests for ordersListReducer

--- src/__fixtures__/orders.ts
+++ src/__fixtures__/orders.ts
@@ -1,7 +1,6 @@
-import { TOrder } from '../services/types';
-import { TApiOrderStatus } from '../services/types/api';
+import { TOrder, TOrders } from '../services/types';
+import { TApiOrders, TApiOrderStatus } from '../services/types/api';
 
-// eslint-disable-next-line import/prefer-default-export
 export const order9932: TOrder = {
     id: '6207d6936d7cd8001b2d5a91',
     number: 9932,
@@ -14,4 +13,84 @@ export const order9932: TOrder = {
         '60d3b41abdacab0026a733ce',
         '60d3b41abdacab0026a733c6',
     ],
+};
+
+export const order9934: TOrder = {
+    id: '6207e1e66d7cd8001b2d5af7',
+    number: 9934,
+    name: 'Spicy флюоресцентный бургер',
+    status: TApiOrderStatus.Pending,
+    createdAt: new Date('2022-02-12T16:35:50.383Z'),
+    updatedAt: new Date('2022-02-12T16:35:50.614Z'),
+    ingredients: [
+        '60d3b41abdacab0026a733c7',
+        '60d3b41abdacab0026a733cc',
+        '60d3b41abdacab0026a733c7',
+    ],
+};
+
+export const order9935: TOrder = {
+    id: '6207e2306d7cd8001b2d5af9',
+    number: 9935,
+    name: 'Краторный люминесцентный бургер',
+    status: TApiOrderStatus.Created,
+    createdAt: new Date('2022-02-12T16:37:04.209Z'),
+    updatedAt: new Date('2022-02-12T16:37:04.448Z'),
+    ingredients: [
+        '60d3b41abdacab0026a733c6',
+        '60d3b41abdacab0026a733c8',
+        '60d3b41abdacab0026a733c6',
+    ],
+};
+
+export const orders: TOrders = [
+    order9935,
+    order9934,
+    order9932,
+];
+
+export const apiOrders: TApiOrders = {
+    orders: [
+        {
+            _id: '6207d6936d7cd8001b2d5a91',
+            number: 9932,
+            name: 'Традиционный-галактический краторный бургер',
+            status: TApiOrderStatus.Done,
+            createdAt: '2022-02-12T15:47:31.913Z',
+            updatedAt: '2022-02-12T15:47:32.195Z',
+            ingredients: [
+                '60d3b41abdacab0026a733c6',
+                '60d3b41abdacab0026a733ce',
+                '60d3b41abdacab0026a733c6',
+            ],
+        },
+        {
+            _id: '6207e1e66d7cd8001b2d5af7',
+            number: 9934,
+            name: 'Spicy флюоресцентный бургер',
+            status: TApiOrderStatus.Pending,
+            createdAt: '2022-02-12T16:35:50.383Z',
+            updatedAt: '2022-02-12T16:35:50.614Z',
+            ingredients: [
+                '60d3b41abdacab0026a733c7',
+                '60d3b41abdacab0026a733cc',
+                '60d3b41abdacab0026a733c7',
+            ],
+        },
+        {
+            _id: '6207e2306d7cd8001b2d5af9',
+            number: 9935,
+            name: 'Краторный люминесцентный бургер',
+            status: TApiOrderStatus.Created,
+            createdAt: '2022-02-12T16:37:04.209Z',
+            updatedAt: '2022-02-12T16:37:04.448Z',
+            ingredients: [
+                '60d3b41abdacab0026a733c6',
+                '60d3b41abdacab0026a733c8',
+                '60d3b41abdacab0026a733c6',
+            ],
+        },
+    ],
+    total: 9848,
+    totalToday: 17,
 };
---
+++ src/services/reducers/__tests__/orders-list.test.ts
@@ -0,0 +1,56 @@
+import { ordersListReducer, TOrdersListState } from '../orders-list';
+import {
+    ORDERS_LIST_CONNECTION_CLOSED,
+    ORDERS_LIST_CONNECTION_ERROR,
+    ORDERS_LIST_CONNECTION_SUCCESS,
+    ORDERS_LIST_RECEIVED,
+} from '../../constants/orders-list';
+import { IOrdersListReceivedAction } from '../../types/actions/orders-list';
+import { apiOrders, orders } from '../../../__fixtures__/orders';
+
+const initialState: TOrdersListState = ordersListReducer(undefined, {} as any);
+
+describe('orders list reducer', () => {
+    it('should return the initial state', () => {
+        expect(initialState).toMatchSnapshot();
+    });
+
+    it('should handle ORDERS_LIST_CONNECTION_SUCCESS', () => {
+        expect(ordersListReducer(undefined, { type: ORDERS_LIST_CONNECTION_SUCCESS }))
+            .toMatchSnapshot();
+    });
+
+    it('should handle ORDERS_LIST_RECEIVED', () => {
+        const action: IOrdersListReceivedAction = {
+            type: ORDERS_LIST_RECEIVED,
+            data: apiOrders,
+        };
+        const state: TOrdersListState = {
+            ...initialState,
+            isConnected: true,
+        };
+        expect(ordersListReducer(state, action))
+            .toMatchSnapshot();
+    });
+
+    it('should handle ORDERS_LIST_CONNECTION_ERROR', () => {
+        expect(ordersListReducer(undefined, { type: ORDERS_LIST_CONNECTION_ERROR, message: null }))
+            .toMatchSnapshot();
+        expect(ordersListReducer(undefined, { type: ORDERS_LIST_CONNECTION_ERROR, message: 'Connection error' }))
+            .toMatchSnapshot();
+    });
+
+    it('should handle ORDERS_LIST_CONNECTION_CLOSED', () => {
+        const state: TOrdersListState = {
+            ...initialState,
+            orders,
+            total: 9848,
+            totalToday: 17,
+            isConnected: true,
+        };
+        expect(ordersListReducer(state, { type: ORDERS_LIST_CONNECTION_CLOSED, message: null }))
+            .toMatchSnapshot();
+        expect(ordersListReducer(state, { type: ORDERS_LIST_CONNECTION_CLOSED, message: 'Connection closed error' }))
+            .toMatchSnapshot();
+    });
+});
