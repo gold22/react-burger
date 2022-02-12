a3f084b8928092056a7fbfc682d5a3d28a0b1e73
Denis Goleshchikhin <dgoleshchikhin@yandex.ru>
2022-02-12 23:14:14 +0700

    Add unit-tests for orderInfoReducer

---
+++ src/__fixtures__/orders.ts
@@ -0,0 +1,17 @@
+import { TOrder } from '../services/types';
+import { TApiOrderStatus } from '../services/types/api';
+
+// eslint-disable-next-line import/prefer-default-export
+export const order9932: TOrder = {
+    id: '6207d6936d7cd8001b2d5a91',
+    number: 9932,
+    name: 'Традиционный-галактический краторный бургер',
+    status: TApiOrderStatus.Done,
+    createdAt: new Date('2022-02-12T15:47:31.913Z'),
+    updatedAt: new Date('2022-02-12T15:47:32.195Z'),
+    ingredients: [
+        '60d3b41abdacab0026a733c6',
+        '60d3b41abdacab0026a733ce',
+        '60d3b41abdacab0026a733c6',
+    ],
+};
